cea7042d2f1202a1a56f9f97dd4a64dd20b4ebb4
wesselanton <wesselant@gmail.com>
2026-07-13 00:56:29 +0200

    Add asteroid spawning logic and implement AsteroidField class

---
+++ asteroid.py
@@ -0,0 +1,15 @@
+import pygame
+from constants import *
+from circleshape import CircleShape
+
+
+class Asteroid(CircleShape):
+    def __init__(self, x: float, y: float, radius: float) -> None:
+        super().__init__(x, y, radius)
+
+    def draw(self, screen: pygame.Surface) -> None:
+        pygame.draw.circle(screen, "white", self.position,
+                           self.radius, width=LINE_WIDTH)
+
+    def update(self, dt: float) -> None:
+        self.position += self.velocity * dt
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
+from asteroid import Asteroid
 from constants import *
 from logger import log_state
 from player import Player
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -9,7 +11,12 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    asteroid_field = AsteroidField()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
