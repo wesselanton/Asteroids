9e22712d88e72b2eda46fcc41aba97eb7211b301
wesselanton <wesselant@gmail.com>
2026-07-12 17:13:09 +0200

    Refactor player update logic and implement rotation functionality

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    screen.fill("black")
     clock = pygame.time.Clock()
     dt = 0.0
     while True:
@@ -21,9 +20,11 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+        screen.fill("black")
+        player.update(dt)
         player.draw(screen)
-        dt = clock.tick(60) / 1000
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
 
 
 class Player(CircleShape):
@@ -11,9 +11,6 @@ class Player(CircleShape):
     def draw(self, screen: pygame.Surface) -> None:
         pygame.draw.polygon(screen, "white", self.triangle(), width=LINE_WIDTH)
 
-    def update(self, dt: float) -> None:
-        self.position += self.velocity * dt
-
     def triangle(self) -> list[pygame.Vector2]:
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(
@@ -22,3 +19,13 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+
+    def rotate(self, dt: float) -> None:
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt: float) -> None:
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
