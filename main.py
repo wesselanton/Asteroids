9f8c85939fdb323ef20acbce195912e584646a4f
wesselanton <wesselant@gmail.com>
2026-07-12 16:52:54 +0200

    Implement main game loop and initialize pygame display

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import pygame
 from constants import *
+from logger import log_state
 
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen.fill("black")
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+    pygame.display.flip()
 
 
 if __name__ == "__main__":
