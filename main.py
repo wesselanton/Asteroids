4738fcb0e088e565770c4defa362aee31e3e547a
wesselanton <wesselant@gmail.com>
2026-07-12 17:07:27 +0200

    Add player and circle shape classes; implement player drawing and updating logic

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen.fill("black")
+    clock = pygame.time.Clock()
+    dt = 0.0
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
-    pygame.display.flip()
+        player.draw(screen)
+        dt = clock.tick(60) / 1000
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
