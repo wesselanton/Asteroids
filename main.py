d4f3e98d1f66489246774c6c0745c59bc4d15311
wesselanton <wesselant@gmail.com>
2026-07-15 21:44:51 +0200

    Implement collision handling between shots and asteroids

--- main.py
+++ main.py
@@ -44,6 +44,11 @@ def main():
         updatable.update(dt)
 
         for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
             if player.collides_with(asteroid):
                 log_event("player_hit")
                 print("Game over!")
